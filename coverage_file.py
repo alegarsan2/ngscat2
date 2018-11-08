1e5578766b14b07f4f6168e46d2abdf3d8d9e3d7
Alejandro G <alegarsan2@gmail.com>
2018-11-08 10:47:29 +0100

    Added new methods for get Data to coverage_file class

--- coverage_file.py
+++ coverage_file.py
@@ -2,41 +2,42 @@
 import numpy as np
 
 class coverage_file():
+
     def __init__(self, filename):
-        # with open(filename,"r") as f:
-        #     chromindex = []
-        #     data = np.empty(shape=(0,4))
-        #
-        #     for line in f.readlines():
-        #         line = line.rstrip()
-        #         part = line.split()
-        #
-        #         if part[0] not in chromindex:
-        #             chromindex.append(part[0])
-        #
-        #         partindex = chromindex.index(part[0])
-        #         row = [partindex, part[1],part[2],part[3]]
-        #         print(row)
-        #         data.append([row],axis= 0)
-        #
-        # self.data = data
-        # self.chromindex = chromindex
 
         lineCount = 0
         with open(filename, "r") as f:
             for line in f.readlines():
                 lineCount = lineCount + 1
 
-        # chromindex = []
-        # data = np.empty(shape=(lineCount, 4))
-        # idx = 0
-        # with open(filename, "r") as f:
-        #     for line in f.readlines():
-        #         part = line.rstrip().split()
-        #         if part[0] not in chromindex:
-        #             chromindex.append(part[0])
-        #         partindex = chromindex.index(part[0])
-        #         row = [partindex, part[1],part[2],part[3]]
-        #         # print(row)
-        #         for jdx in range(0,4):
-        #             data[idx, jdx] = row[jdx]
+        chromindex = []
+        data = np.empty(shape=(lineCount, 4),dtype= int)
+        idx = 0
+        with open(filename, "r") as f:
+            for idx,line in enumerate(f.readlines()):
+                part = line.rstrip().split()
+                if part[0] not in chromindex:
+                    chromindex.append(part[0])
+
+                partindex = chromindex.index(part[0])
+                row = [partindex, part[1],part[2],part[3]]
+
+                data[idx,:] = row
+
+        self.data = data
+        self.chromindex = chromindex
+
+
+    def getInit(self):
+        return(self.data[:,1])
+
+    def getEnd(self):
+        return(self.data[:,2])
+
+    def getCov(self):
+        return(self.data[:,3])
+
+    def getChrom(self):
+        chromlist = [self.chromindex[i] for i in self.data[:,0]]
+
+        return(chromlist)
