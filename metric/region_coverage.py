6f661c52a873860494011a91e828205a25c3e038
Alejandro G <alegarsan2@gmail.com>
2018-12-05 13:20:49 +0100

    Added Class for Regions with Zero Output Added Class for Std regions report

--- metric/region_coverage.py
+++ metric/region_coverage.py
@@ -1,6 +1,6 @@
 import numpy as np
 from plotly import graph_objs as go
-from coverage_file import ZeroProcessor
+
 import math
 def getChromosomeNames(coverage):
     chromosomeNames = []
@@ -168,36 +168,138 @@ def renderCoveragePerChr(coverage, chromosomeName, windowsize):
     return trace
 
 
-# def processZero(chromosome, region):
-#     zeroregion = []
-#     initzero = None
-#     regionlen = region.end - region.start
-#
-#     for idx, basecoverage in enumerate(region.basecoverage):
-#         if basecoverage == 0 and idx != regionlen:
-#             if initzero is None:
-#                 initzero = idx
-#         else:
-#             zeroregion.append([chromosome, initzero + region.start, idx + region.start])
-#             initzero = None
-#
-#     return zeroregion
+
+class RegionsWithZeroesProcessor():
+
+    def __init__(self,coveragefilecoverages ,path):
+        self.file = open(path, "w")
+        self.coverages = coveragefilecoverages
+
+    def process(self, chromosome, region):
+        zeroregion = []
+        initzero = None
+        rinit = region.start
+        regionlen = region.end - region.start
+        regionidx = region.covStartIndex
+        regioni = self.coverages[region.covStartIndex:region.covEndIndex]
+        for idx, basecoverage in enumerate(regioni):
+            if idx != regionlen:
+                if basecoverage == 0:
+                    if initzero is None:
+                        initzero = idx
+                else:
+                    if initzero is not None:
+                        self.file.write("\t".join(map(str,[chromosome.name, initzero + region.start, idx + region.start])) + "\n")
+                        initzero = None
+            else:
+                if basecoverage == 0 and initzero is not None:
+                    self.file.write("\t".join(map(str,[chromosome.name, initzero + region.start, idx + region.start]))+ "\n")
+                else:
+                    self.file.write("\t".join(map(str,[chromosome.name, idx + region.start, idx + region.start]))+ "\n")
+    def close(self):
+        self.file.close()
+
 
 def zeroCoverageRegions(coveragefile, outdir):
-    '''Generate bamfile of regions within targets that have Zero coverage.
+    '''Generate Nocoverage.txt of regions within targets that have Zero coverage.
     Input: Coveragefile object, outdir '''
 
 
-    zerosProcessor = ZeroProcessor(coveragefile.coverages)
-    #coveragefile.iterateOverRegions(zerosProcessor.process)
-
-    fdw_zero = open(outdir + "NoCoverage.txt", 'w')
-    #fdw_zero.write("\n".join("\t".join(coveragefile.iterateOverRegions(zerosProcessor.process))))
-    #fdw_zero.write("".join(map(str,[coveragefile.iterateOverRegions(zerosProcessor.process) if coveragefile.iterateOverRegions(zerosProcessor.process) is not None else 'hola'])))
-    x = []
-    x.append(coveragefile.iterateOverRegions(zerosProcessor.process))
-    print('hola')
+    zerosProcessor = RegionsWithZeroesProcessor(coveragefile.coverages, outdir + "NoCoverage.txt")
+    coveragefile.iterateOverRegions(zerosProcessor.process)
+    zerosProcessor.close()
 
 
+def processRegionStd(chromosome,region):
+    if region.mean > 0:
+        return region.std
 
-#def region_std_distribution(coveragefile, outdir):
+# def region_std_distribution(coveragefiles, outdir, legend=None, bins='auto', warnthreshold = 40):
+#     # Histo_CV sustitute.
+#     region_stddistribution_result = {}
+#     results = []
+#     histlist = []
+#     percentile = []
+#     maximum = []
+#     minimum = []
+#     mean = []
+#     median= []
+#     zerocov = []
+#
+#
+#
+#     for i, coveragefile in enumerate(coveragefiles):
+#         stdlist = []
+#         #Non zero indexs
+#         stdlist = coveragefile.iterateOverRegions(processRegionStd)
+#         #FIXME no existe normalizacion de tamaño de bins, solo toma segun el tamaño de la muestra(como es sin 0) QUitar 0??
+#         number_read, bin_edges = np.histogram(coveragefile.coverages[indnonzero], bins= bins)
+#         bin_edges = bin_edges.tolist()
+#
+#         width = []
+#         xaxis = []
+#         for i in range(len(bin_edges)-1):
+#             xaxis.append((bin_edges[i]+ bin_edges[i +1])/2)
+#             width.append(bin_edges[1]- bin_edges[0])
+#
+#         histlist.append(dict([('numberread', number_read.tolist()),
+#                               ('binedges', bin_edges),
+#                               ('coveragepos', [round(x,2) for x in xaxis]),
+#                               ('width', width)]))
+#
+#         percentile.append(dict(
+#             [('Q1', np.percentile(coveragefile.coverages, 25)),
+#             ('Q2', np.percentile(coveragefile.coverages, 50)),
+#             ('Q3', np.percentile(coveragefile.coverages, 75))
+#             ]))
+#         median.append(np.median(coveragefile.coverages))
+#         maximum.append(np.max(coveragefile.coverages))
+#         minimum.append(np.min(coveragefile.coverages))
+#         mean.append(coveragefile.coverages.mean())
+#         zerocov.append(len(coveragefile.coverages) - len(indnonzero))
+#         #TODO ver que parametros metemos en el JSON
+#     for i in range(len(bamlist)):
+#         results.append(dict(
+#             [('bamfilename', bamlist[i].filename.decode('utf-8')),
+#              ('legend', (legend[i] if legend is not None else bamlist[i].filename.decode('utf-8').split('/')[-1])),
+#              ('histdata', histlist[i]),
+#              ('percentile',percentile[i]),
+#              ('max', float(maximum[i])),
+#              ('min', float(minimum[i])),
+#              ('mean', mean[i]),
+#              ('median', median[i]),
+#              ('zerocov', float(zerocov[i])),
+#              ('status', 'OK' if mean[i] >= warnthreshold else 'Not OK')]
+#         ))
+#
+#     region_stddistribution_result['results'] = results
+#     region_stddistribution_result['bins'] = bins
+#     region_stddistribution_result['warnthreshold'] = warnthreshold
+#     region_stddistribution_result['outdir'] = outdir
+#
+#     with open(outdir + '/region_stddistribution_result.json', 'w') as outfile:
+#         json.dump(region_stddistribution_result, outfile)
+#
+#     return region_stddistribution_result, coveragefiles
+
+
+class StdReport():
+    def __init__(self, coveragefile,warnthreshold):
+        self.stdlist = coveragefile.iterateOverRegions(self.process)
+        self.results = self.calculate_results(self,coveragefile,warnthreshold)
+    def process(self,chromosome,region):
+        if region.mean > 0:
+            self.stdlist.append(region.std)
+    def calculate_results(self,coveragefile,warnthreshold):
+        #Plot here
+    def std_distr_hist(self,outdir):
+        #Plot His
+    def std_distr_box(self,outdir):
+        #Plot
+    def std_distr_json(self,outdir):
+        #Dump self.results
+    def write(self,outdir):
+        self.stdDistrBox(outdir)
+        self.stdDistrBox(outdir)
+        self.stdDistrJson(outdir)
+
