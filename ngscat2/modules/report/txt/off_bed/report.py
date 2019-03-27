ce177a94bcf6768b5e545201d19405113c2a6cc3
Alejandro G <alegarsan2@gmail.com>
2019-03-27 12:48:11 +0100

    Added Annotation function

--- ngscat2/modules/report/txt/off_bed/report.py
+++ ngscat2/modules/report/txt/off_bed/report.py
@@ -6,8 +6,7 @@ class Report():
         self.outdir = outdir + "/data"
 
     def report(self, offset, coverageThreshold, target, annotation = None):
-        '''Generate Nocoverage.txt of regions within targets that have Zero coverage.
-        Input: Coveragefile object, outdir '''
+        '''Calculation of the off target regions within thresholds and annotation using bedops and a .bed annotated'''
         bedgraphlist = [filename for filename in os.listdir(self.outdir) if ".bed" in filename]
 
         for bedgraph in bedgraphlist:
@@ -17,13 +16,7 @@ class Report():
         if annotation is not None:
             bedlist = [filename for filename in os.listdir(self.outdir) if ".bed" in filename]
             for bedname in bedlist:
-                directory = os.path.join(self.outdir, bedname)
-                directory2 = os.path.join(self.outdir, bedname.replace(".bed", "annotated.bed"))
-                print("bedmap --echo --delim \'\\t\' --echo-map-id-uniq " + os.path.join(self.outdir, bedname) \
-                            + " " + annotation + " > " + os.path.join(self.outdir, bedname.replace(".bed", ".annotated.bed")))
-
                 subprocess.run("bedmap --echo --delim \'\\t\' --echo-map-id-uniq " + os.path.join(self.outdir, bedname) \
                             + " " + annotation + " > " + os.path.join(self.outdir, bedname.replace(".bed", ".annotated.bed")), shell = True)
                 os.remove(os.path.join(self.outdir, bedname))
-                # subprocess.run("bedmap --echo --delim \'\\t\' --echo-map-id-uniq " + os.path.join(self.outdir, bedname) \
-                #                             + " " + annotation, shell=True)
+
